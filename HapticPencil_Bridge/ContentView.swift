5f53036be8aa49410f27cec5782cb2b335c36579
Lucio Cuddeford <cuddeford@me.com>
2025-07-09 17:34:37 +0100

    Initial Commit

---
+++ HapticPencil_Bridge/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HapticPencil_Bridge
+//
+//  Created by Lucio Cuddeford on 09/07/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HapticPencil_Bridge/HapticPencil_BridgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HapticPencil_BridgeApp.swift
+//  HapticPencil_Bridge
+//
+//  Created by Lucio Cuddeford on 09/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct HapticPencil_BridgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
